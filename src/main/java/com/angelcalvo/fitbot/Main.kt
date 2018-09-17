7d03a353901dcf6ce30fc1d691e3bd038e8b1766
Angel Calvo <acalvo@tuenti.com>
2018-09-17 23:03:03 +0200

    Webserver in Spark

--- src/main/java/com/angelcalvo/fitbot/Main.kt
+++ src/main/java/com/angelcalvo/fitbot/Main.kt
@@ -3,8 +3,23 @@ package com.angelcalvo.fitbot
 import org.telegram.telegrambots.ApiContextInitializer
 import org.telegram.telegrambots.meta.TelegramBotsApi
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException
+import spark.Spark.get
+import spark.Spark.port
+import kotlin.concurrent.thread
 
 fun main(args: Array<String>) {
+    thread {
+        initBot()
+    }
+
+    println("Starting web server...")
+    port(getHerokuAssignedPort())
+    get("/") { req, res ->
+        "Hello World"
+    }
+}
+
+fun initBot() {
     val token = System.getenv("TG_TOKEN")
     val creatorId = System.getenv("TG_CREATOR_ID").toInt()
     val googleCredentials = System.getenv("GOOGLE_CREDENTIALS")
@@ -16,4 +31,12 @@ fun main(args: Array<String>) {
     } catch (e: TelegramApiException) {
         e.printStackTrace()
     }
+}
+
+fun getHerokuAssignedPort(): Int {
+    val processBuilder = ProcessBuilder()
+    if (processBuilder.environment().get("PORT") != null) {
+        return Integer.parseInt(processBuilder.environment().get("PORT"))
+    }
+    return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
 }
