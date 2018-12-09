579cf1bd8b6aa0a0a880243bc8f75e8271f44f70
Angel Calvo <acalvo@tuenti.com>
2018-12-09 23:34:49 +0100

    Removed warning

--- src/main/java/com/angelcalvo/fitbot/Tester.kt
+++ src/main/java/com/angelcalvo/fitbot/Tester.kt
@@ -3,5 +3,5 @@ package com.angelcalvo.fitbot
 fun main(args: Array<String>) {
     val sheet = GoogleSheetsHandler(System.getenv("GOOGLE_CREDENTIALS"))
 
-    ChartHandler().curerntChart(sheet.currentChart(222426316)!!)
+    ChartHandler().curerntChart(sheet.currentChart(222426316))
 }
